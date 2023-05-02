af8fc66fefb52aa29dc7f17b26d796d2a68a03f8
Guilherme Souza <guirsz@gmail.com>
2023-05-02 14:46:39 -0300

    Handling application exceptions

---
+++ Findox.Api/Findox.Api/Middlewares/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Findox.Api.Domain.Responses;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace Findox.Api.Middlewares
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            var logger = app.ApplicationServices.GetService<ILogger>();
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger?.LogError($"Something went wrong: {contextFeature.Error}");
+                        await context.Response.WriteAsync(new ErrorDetailsResponse
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error."
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
